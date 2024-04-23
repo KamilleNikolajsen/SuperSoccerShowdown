f42e5d22a25fa2a231fda354e84a6c00491db250
Kamille Nikolajsen <kamille.nikolajsen@lego.com>
2024-04-23 13:36:52 +0200

    fixed naming

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from infrastructure import fetchData
-from Application import teamGenerator
-from Application import displayTeam
+from infrastructure import fetch_data
+from application import team_generator
+from presentation import display_team
 
 
 def print_teams():
-    pokemon_team = teamGenerator.generate_team_v2(fetchData.fetch_pokemon, 'Pokemon Team', 5,
-                                                  'https://pokeapi.co/api/v2/pokemon/', 1, 100)
-    displayTeam.display_team(pokemon_team)
+    pokemon_team = team_generator.generate_team_v2(fetch_data.fetch_pokemon, 'Pokemon Team', 5,
+                                                    1, 100)
+    display_team.display_team(pokemon_team)
 
-    star_wars_team = teamGenerator.generate_team_v2(fetchData.fetch_person, 'Star Wars Team', 5,
-                                                    'https://swapi.py4e.com/api/people/', 1, 100)
-    displayTeam.display_team(star_wars_team)
+    star_wars_team = team_generator.generate_team_v2(fetch_data.fetch_person, 'Star Wars Team', 5,
+                                                     1, 100)
+    display_team.display_team(star_wars_team)
 
 
 if __name__ == '__main__':
